aacb669e04899280f6788d08066967fbb9ea7f55
mabersold <mabersold@yahoo.com>
2023-11-02 21:18:00 -0700

    2023 MLB season data

--- src/main/kotlin/mabersold/models/League.kt
+++ src/main/kotlin/mabersold/models/League.kt
@@ -10,7 +10,7 @@ enum class League(
     val excludeRegularSeason: List<Int>
 ) {
     GENERIC(1, listOf(), 2022, listOf(), 1, 1, listOf()),
-    MLB(1903, listOf(1904, 1994), 2022, listOf(1981), 1969, 1969, listOf()),
+    MLB(1903, listOf(1904, 1994), 2023, listOf(1981), 1969, 1969, listOf()),
     NFL(1933, listOf(), 2022, listOf(), 1966, 1966, listOf()),
     NBA(1946, listOf(), 2022, listOf(), 1970, 1946, listOf()),
     NHL(1926, listOf(2004), 2022, listOf(), 1974, 1926, listOf(2004))

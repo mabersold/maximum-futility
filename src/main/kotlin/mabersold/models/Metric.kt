f8c69550abff5f62df35a7fa38fa8617b961262f
mabersold <mabersold@yahoo.com>
2024-10-29 22:14:52 -0700

    Removing models that are no longer used

--- src/main/kotlin/mabersold/models/Metric.kt
+++
@@ -1,8 +0,0 @@
-package mabersold.models
-
-data class Metric(
-    val total: Int,
-    val opportunities: Int
-) {
-    val rate = if (opportunities > 0) total.toDouble() / opportunities else null
-}

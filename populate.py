612da82ee3c35960090cc28a29e98af6874e718d
mabersold <mabersold@yahoo.com>
2024-12-11 19:42:34 -0800

    Create python version of populate script

---
+++ populate.py
@@ -0,0 +1,119 @@
+import json
+import http.client
+
+# Leagues data to populate
+leagues = [
+    {"name": "MLB", "sport": "Baseball", "label": "mlb"},
+    {"name": "NFL", "sport": "Football", "label": "nfl"},
+    {"name": "NBA", "sport": "Basketball", "label": "nba"},
+    {"name": "NHL", "sport": "Hockey", "label": "nhl"},
+    {"name": "WNBA", "sport": "Basketball", "label": "wnba"},
+    {"name": "MLS", "sport": "Soccer", "label": "mls"}
+]
+
+# Metro areas data to populate
+metros = [
+    {"name": "Anderson", "label": "anderson"},
+    {"name": "Atlanta", "label": "atlanta"},
+    {"name": "Austin", "label": "austin"},
+    {"name": "Baltimore", "label": "baltimore"},
+    {"name": "Calgary", "label": "calgary"},
+    {"name": "Charlotte", "label": "charlotte"},
+    {"name": "Chicago", "label": "chicago"},
+    {"name": "Cincinnati", "label": "cincinnati"},
+    {"name": "Cleveland", "label": "cleveland"},
+    {"name": "Columbus", "label": "columbus"},
+    {"name": "Dallas", "label": "dallas"},
+    {"name": "Decatur", "label": "decatur"},
+    {"name": "Denver", "label": "denver"},
+    {"name": "Detroit", "label": "detroit"},
+    {"name": "Edmonton", "label": "edmonton"},
+    {"name": "Fort Wayne", "label": "fort-wayne"},
+    {"name": "Grand Rapids", "label": "grand-rapids"},
+    {"name": "Green Bay", "label": "green-bay"},
+    {"name": "Hartford", "label": "hartford"},
+    {"name": "Houston", "label": "houston"},
+    {"name": "Indianapolis", "label": "ind"},
+    {"name": "Jacksonville", "label": "jacksonville"},
+    {"name": "Kansas City", "label": "kansas-city"},
+    {"name": "Las Vegas", "label": "las-vegas"},
+    {"name": "Los Angeles", "label": "la"},
+    {"name": "Memphis", "label": "memphis"},
+    {"name": "Miami", "label": "miami"},
+    {"name": "Milwaukee", "label": "milwaukee"},
+    {"name": "Minneapolis", "label": "minn"},
+    {"name": "Montreal", "label": "montreal"},
+    {"name": "Nashville", "label": "nashville"},
+    {"name": "New Orleans", "label": "new-orleans"},
+    {"name": "New York", "label": "ny"},
+    {"name": "Oklahoma City", "label": "okc"},
+    {"name": "Orlando", "label": "orlando"},
+    {"name": "Ottawa", "label": "ottawa"},
+    {"name": "Philadelphia", "label": "phil"},
+    {"name": "Phoenix", "label": "phoenix"},
+    {"name": "Pittsburgh", "label": "pitt"},
+    {"name": "Portland", "label": "pdx"},
+    {"name": "Portsmouth", "label": "portsmouth"},
+    {"name": "Providence", "label": "providence"},
+    {"name": "Quebec", "label": "quebec"},
+    {"name": "Raleigh", "label": "raleigh"},
+    {"name": "Rochester", "label": "rochester"},
+    {"name": "Sacramento", "label": "sacramento"},
+    {"name": "Salt Lake City", "label": "slc"},
+    {"name": "San Antonio", "label": "san-antonio"},
+    {"name": "San Diego", "label": "san-diego"},
+    {"name": "San Francisco Bay", "label": "sf"},
+    {"name": "Seattle", "label": "seattle"},
+    {"name": "Sheboygan", "label": "sheboygan"},
+    {"name": "Sioux City", "label": "sioux-city"},
+    {"name": "St. Louis", "label": "stl"},
+    {"name": "Syracuse", "label": "syracuse"},
+    {"name": "Tampa Bay", "label": "tb"},
+    {"name": "Toronto", "label": "toronto"},
+    {"name": "Tri-Cities", "label": "tri-cities"},
+    {"name": "Tulsa", "label": "tulsa"},
+    {"name": "Vancouver", "label": "vancouver"},
+    {"name": "Washington", "label": "dc"},
+    {"name": "Winnipeg", "label": "winnipeg"}
+]
+
+# Populate Leagues
+print("Populating Leagues...")
+
+BASE_URL = 'localhost:8080'
+
+league_labels_to_ids = {}
+
+print("Populating leagues")
+for league in leagues:
+    conn = http.client.HTTPConnection(BASE_URL)
+    conn.request("POST", "/api/v1/leagues", body=json.dumps(league), headers={'Content-Type': 'application/json'})
+    response = conn.getresponse()
+
+    if response.status != 200:
+        print("Failed to create league, for reasons.")
+        continue
+
+    response_body = json.loads(response.read())
+    league_id = response_body['id']
+    label = response_body['label']
+    league_labels_to_ids[label] = league_id
+
+metro_labels_to_ids = {}
+print("Populating metros")
+for metro in metros:
+    conn = http.client.HTTPConnection(BASE_URL)
+    conn.request("POST", "/api/v1/metros", body=json.dumps(metro), headers={'Content-Type': 'application/json'})
+
+    response = conn.getresponse()
+
+    if response.status != 200:
+        print(f"Failed to create metro {metro['name']}")
+        continue
+
+    response_body = json.loads(response.read())
+    metro_id = response_body['id']
+    label = response_body['label']
+    metro_labels_to_ids[label] = metro_id
+
+print("Done!")
